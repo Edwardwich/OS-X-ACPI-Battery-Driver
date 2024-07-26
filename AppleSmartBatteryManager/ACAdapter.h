f29cdb03b144982940ad0d304f06c2827b16a8fa
ali ghafourian <35195176+Edwardwich@users.noreply.github.com>
2024-07-27 00:25:21 +0330

    Update ACAdapter.h

--- AppleSmartBatteryManager/ACAdapter.h
+++ AppleSmartBatteryManager/ACAdapter.h
@@ -9,6 +9,12 @@
 #ifndef ACPIBatteryManager_ACAdapter_h
 #define ACPIBatteryManager_ACAdapter_h
 
+#include <IOKit/IOService.h>
+#include <IOKit/IOReportTypes.h>
+#include <IOKit/acpi/IOACPIPlatformDevice.h>
+#include <IOKit/pwr_mgt/IOPMPowerSource.h>
+#include <IOKit/pwr_mgt/RootDomain.h>
+#include <IOKit/IOTimerEventSource.h>
 #include "AppleSmartBatteryManager.h"
 
 class EXPORT ACPIACAdapter : public IOService
@@ -20,22 +26,25 @@ private:
     IOWorkLoop*             fWorkloop;
     IOCommandGate*          fCommandGate;
     IORecursiveLock*        fLock;
-    
+    IOACPIPlatformDevice    *device;
+    IOSimpleLock            *lock;
+    IOACPIAddress           *info;
     IONotifier*             fPublishNotify;
     IONotifier*             fTerminateNotify;
     
     OSSet*                  fBatteryServices;
     
     bool                    fACConnected;
-
+     
     void                    gatedHandler(IOService* newService, IONotifier * notifier);
     bool                    notificationHandler(void * refCon, IOService * newService, IONotifier * notifier);
     void                    pollState();
 public:
+    ACPIACAdapter(IOACPIPlatformDevice *device, int32_t id, IOSimpleLock *lock, IOACPIAddress *info) ;
     virtual bool            init(OSDictionary* dict);
     virtual bool            start(IOService* provider);
     virtual void            stop(IOService* provider);
-    virtual IOReturn        setPowerState(unsigned long state, IOService* device);
+    virtual IOReturn        setPowerState(unsigned long state, IOService* device );
     virtual IOReturn        message(UInt32 type, IOService* provider, void* argument);
 };
 
